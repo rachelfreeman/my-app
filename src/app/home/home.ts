bfd5ef71667f5ce371d88f904af6f64feacb780b
Kevin Freeman <mrskevinfreeman@gmail.com>
2025-09-23 12:05:00 -0500

    add input and button to home component

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   imports: [],
-  templateUrl: './home.html',
-  styleUrl: './home.css'
+  styleUrl: './home.css',
+  template: `
+    <section>
+      <form>
+        <input type="text" placeholder="Enter a to do item to add" />
+        <button class="primary" type="button">Add</button>
+      </form>
+    </section>
+  `,
 })
 export class Home {
 
