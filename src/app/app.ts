703a4242a1b0b34d8bd1fbe4e9fd12d54bf90d33
Kevin Freeman <mrskevinfreeman@gmail.com>
2025-09-23 11:56:43 -0500

    initialize angular app with 1 home component

---
+++ src/app/app.ts
@@ -0,0 +1,22 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {Home} from './home/home';
+
+@Component({
+  selector: 'app-root',
+  imports: [Home],
+  styleUrl: './app.css',
+  template: `
+    <main>
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
+  `,
+})
+export class App {
+  protected readonly title = signal('my-app');
+}
